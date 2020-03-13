a8e50f00d476b583a88746a8571bcff5f956f890
fpuentes <fpuentes@ujaen.es>
2020-03-13 13:04:28 +0100

    Part 1 of day 10

---
+++ Day 10/part1.py
@@ -0,0 +1,149 @@
+import numpy as np
+
+
+def get_point_asteroids(map):
+    asteroids=[]
+    rows=len(map)
+    cols=len(map[0])
+
+    for x in range(rows):
+        for y in range(cols):
+            if(map[x][y] == '#'):
+                asteroids.append([x,y])
+
+    return asteroids
+
+def asteroids_detected(asteroids,point):
+    sight_lines=[]
+
+    for asteroid in asteroids:
+        if(asteroid != point):
+            var_x=float(asteroid[0]-point[0])
+            var_y=float(asteroid[1]-point[1])
+            sign_x=sign_y=''
+
+            if(var_x != 0):
+                p=var_y/var_x
+            elif(var_y > 0):
+                p=float('inf')
+            else:
+                p=float('-inf')
+
+            sign_x = '+' if var_x >= 0 else '-'
+            sign_y = '+' if var_y >= 0 else '-'
+
+            key=sign_x+sign_y+p.__str__()
+            sight_lines.append(key)
+
+    return len(set(sight_lines))
+
+
+def get_best_asteroid(asteroids):
+    solutions=[]
+
+    for asteroid in asteroids:
+            solutions.append(asteroids_detected(asteroids,asteroid))
+
+    return np.max(solutions)
+
+
+
+# Examples
+asteroid_map=[
+    '.#..#',
+    '.....',
+    '#####',
+    '....#',
+    '...##'
+]
+asteroids=get_point_asteroids(asteroid_map)
+print(get_best_asteroid(asteroids))
+
+
+asteroid_map=[
+    '......#.#.',
+    '#..#.#....',
+    '..#######.',
+    '.#.#.###..',
+    '.#..#.....',
+    '..#....#.#',
+    '#..#....#.',
+    '.##.#..###',
+    '##...#..#.',
+    '.#....####'
+]
+asteroids=get_point_asteroids(asteroid_map)
+print(get_best_asteroid(asteroids))
+
+
+asteroid_map=[
+    '#.#...#.#.',
+    '.###....#.',
+    '.#....#...',
+    '##.#.#.#.#',
+    '....#.#.#.',
+    '.##..###.#',
+    '..#...##..',
+    '..##....##',
+    '......#...',
+    '.####.###.'
+]
+asteroids=get_point_asteroids(asteroid_map)
+print(get_best_asteroid(asteroids))
+
+
+asteroid_map=[
+    '.#..#..###',
+    '####.###.#',
+    '....###.#.',
+    '..###.##.#',
+    '##.##.#.#.',
+    '....###..#',
+    '..#.#..#.#',
+    '#..#.#.###',
+    '.##...##.#',
+    '.....#.#..'
+]
+asteroids=get_point_asteroids(asteroid_map)
+print(get_best_asteroid(asteroids))
+
+
+asteroid_map=[
+    '.#..##.###...#######',
+    '##.############..##.',
+    '.#.######.########.#',
+    '.###.#######.####.#.',
+    '#####.##.#.##.###.##',
+    '..#####..#.#########',
+    '####################',
+    '#.####....###.#.#.##',
+    '##.#################',
+    '#####.##.###..####..',
+    '..######..##.#######',
+    '####.##.####...##..#',
+    '.#####..#.######.###',
+    '##...#.##########...',
+    '#.##########.#######',
+    '.####.#.###.###.#.##',
+    '....##.##.###..#####',
+    '.#.#.###########.###',
+    '#.#.#.#####.####.###',
+    '###.##.####.##.#..##'
+]
+asteroids=get_point_asteroids(asteroid_map)
+print(get_best_asteroid(asteroids))
+
+
+# My puzzle
+print("Result for my puzzle:")
+# Load data
+file = open('data/input.data', 'r')
+lines = file.readlines()
+asteroid_map=[line[:-1] for line in lines]
+
+# Calculate the solution
+asteroids=get_point_asteroids(asteroid_map)
+solution=get_best_asteroid(asteroids)
+
+# Print the solution
+print("Solution: "+solution.__str__())
