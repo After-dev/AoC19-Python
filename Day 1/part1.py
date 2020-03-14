58cc74193e1c440fdf753f9cd855ab32682ebaf3
fpuentes <fpuentes@ujaen.es>
2020-03-15 00:49:44 +0100

    Day 1. Clean up

--- Day 1/part1.py
+++ Day 1/part1.py
@@ -3,22 +3,22 @@ import numpy as np
 
 def compute_fuel(mass):
     fuel_required=np.floor(mass/3)-2
-    print("Fuel requirements for a module with "+mass.__str__()+" of mass: "+fuel_required.__str__())
+    #print("Fuel requirements for a module with "+mass.__str__()+" of mass: "+fuel_required.__str__())
     return fuel_required
 
 
 # Examples
 print("Result for examples:")
-compute_fuel(12)
-compute_fuel(14)
-compute_fuel(1969)
-compute_fuel(100756)
+print(compute_fuel(12))
+print(compute_fuel(14))
+print(compute_fuel(1969))
+print(compute_fuel(100756))
 
 
 # My puzzle
 print("Result for my puzzle:")
 # Load data
-file = open('data/input.data', 'r')
+file = open('./input.data', 'r')
 lines = file.readlines()
 modules=[int(mod[:-1]) for mod in lines]
 
@@ -28,4 +28,4 @@ for mod in modules:
     solution += compute_fuel(mod)
 
 # Print the solution
-print("Solution: "+solution.__str__())
+print(solution)
--- Day 1/part2.py
+++ Day 1/part2.py
@@ -2,38 +2,34 @@ import numpy as np
 
 
 # Recursive calculate total mass for a module
-def compute_fuel2(mass):
+def compute_fuel(mass):
     fuel=np.floor(mass/3)-2
 
     if(fuel <= 0):
         return 0
 
-    return fuel+compute_fuel2(fuel)
+    return fuel+compute_fuel(fuel)
 
 
 # Examples
 print("Result for examples:")
-e1=compute_fuel2(14)
-e2=compute_fuel2(1969)
-e3=compute_fuel2(100756)
-print("Fuel requirements for a module with 14 of mass: "+e1.__str__())
-print("Fuel requirements for a module with 1969 of mass: "+e2.__str__())
-print("Fuel requirements for a module with 100756 of mass: "+e3.__str__())
+print(compute_fuel(14))
+print(compute_fuel(1969))
+print(compute_fuel(100756))
 
 
 # my puzzle
 print("Result for my puzzle:")
 # Load data
-file = open('data/input.data', 'r')
+file = open('./input.data', 'r')
 lines = file.readlines()
 modules=[int(mod[:-1]) for mod in lines]
 
 # Calculate the solution
 solution=0
 for mod in modules:
-    fuel_required=compute_fuel2(mod)
-    print("Fuel requirements for a module with "+mod.__str__()+" of mass: "+fuel_required.__str__())
+    fuel_required=compute_fuel(mod)
     solution += fuel_required
 
 # Print the solution
-print("Solution: "+solution.__str__())
+print(solution)
