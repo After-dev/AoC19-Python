dddc9fa26bb985f289f8bdf1fb7cdd3c9c63685b
fpuentes <fpuentes@ujaen.es>
2020-03-14 19:42:07 +0100

    Part 1 of day 12

---
+++ Day 12/part1.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+
+def motion_simulation(init_pos_moons,steps):
+    moons_pos=init_pos_moons[:]
+
+    # Init moon velocities
+    moons_velocities=[]
+    for _ in init_pos_moons:
+        moons_velocities.append([0,0,0])
+
+    # Print position and velocity of each moon at init
+    print('After 0 steps:')
+    for i in range(len(moons_pos)):
+        print('pos=<x='+str(moons_pos[i][0])+', y='+str(moons_pos[i][1])+', z='+str(moons_pos[i][2])+'>, vel=<x='+str(moons_velocities[i][0])+', y='+str(moons_velocities[i][0])+', z='+str(moons_velocities[i][0])+'>')
+    print
+
+    # Simulate each step
+    for step in range(steps):
+        # Update velocities (compare every pair of moons)
+        for i,moon1 in enumerate(moons_pos,start=0):
+            for moon2 in moons_pos:
+                if(moon1 != moon2):
+                    for dim in range(3):
+                        if(moon1[dim] > moon2[dim]):
+                            moons_velocities[i][dim] += -1
+                        elif(moon1[dim] < moon2[dim]):
+                            moons_velocities[i][dim] += 1
+
+        # Update positions
+        for i in range(len(moons_pos)):
+            for dim in range(3):
+                moons_pos[i][dim] += moons_velocities[i][dim]
+
+        # Print position and velocity of each moon
+        print('After '+str(step+1)+' steps:')
+        for i in range(len(moons_pos)):
+            print('pos=<x='+str(moons_pos[i][0])+', y='+str(moons_pos[i][1])+', z='+str(moons_pos[i][2])+'>, vel=<x='+str(moons_velocities[i][0])+', y='+str(moons_velocities[i][1])+', z='+str(moons_velocities[i][2])+'>')
+        print
+
+    return [moons_pos,moons_velocities]
+
+def calculate_total_energy(moons_pos,moons_velocities):
+    total_energy=0
+
+    for i in range(len(moons_pos)):
+        pot_energy=kin_energy=0
+        for dim in range(3):
+            pot_energy += abs(moons_pos[i][dim])
+            kin_energy += abs(moons_velocities[i][dim])
+
+        total_energy += pot_energy*kin_energy
+
+    return total_energy
+
+
+
+# Examples
+init_pos_Io=[-1,0,2]
+init_pos_Europa=[2,-10,-7]
+init_pos_Ganymede=[4,-8,8]
+init_pos_Callisto=[3,5,-1]
+init_pos_moons=[init_pos_Io,init_pos_Europa,init_pos_Ganymede,init_pos_Callisto]
+[moons_pos,moons_velocities]=motion_simulation(init_pos_moons,steps=10)
+print(calculate_total_energy(moons_pos,moons_velocities))
+
+
+init_pos_Io=[-8,-10,0]
+init_pos_Europa=[5,5,10]
+init_pos_Ganymede=[2,-7,3]
+init_pos_Callisto=[9,-8,-3]
+init_pos_moons=[init_pos_Io,init_pos_Europa,init_pos_Ganymede,init_pos_Callisto]
+[moons_pos,moons_velocities]=motion_simulation(init_pos_moons,steps=100)
+print(calculate_total_energy(moons_pos,moons_velocities))
+
+
+
+
+# My puzzle
+print("Result for my puzzle:")
+# Input data
+init_pos_moons=[
+    [5,13,-3],
+    [18,-7,13],
+    [16,3,4],
+    [0,8,8]
+]
+
+[moons_pos,moons_velocities]=motion_simulation(init_pos_moons,steps=1000)
+
+# Calculate the solution
+solution=calculate_total_energy(moons_pos,moons_velocities)
+
+# Print the solution
+print("Solution: "+solution.__str__())
