2a33ab590554c635e9383c45dee31ea91924ae5a
fpuentes <fpuentes@ujaen.es>
2020-03-15 18:39:41 +0100

    Day 10. Clean up

--- Day 10/part1.py
+++ Day 10/part1.py
@@ -1,26 +1,27 @@
 import numpy as np
 
 
-def get_point_asteroids(map):
-    asteroids=[]
+# Traduce map to get points in which object appear
+def get_points(map,object):
+    objects=[]
     rows=len(map)
     cols=len(map[0])
 
+    # Find object in each point of the map
     for x in range(rows):
         for y in range(cols):
-            if(map[x][y] == '#'):
-                asteroids.append([x,y])
+            if(map[x][y] == object):
+                objects.append([x,y])
+    return objects
 
-    return asteroids
-
-def asteroids_detected(asteroids,point):
+def asteroids_detected(asteroids,station):
     sight_lines=[]
 
     for asteroid in asteroids:
-        if(asteroid != point):
-            # Calculate down of a straight
-            var_x=float(asteroid[0]-point[0])
-            var_y=float(asteroid[1]-point[1])
+        if(asteroid != station):
+            # Calculate straight
+            var_x=float(asteroid[0]-station[0])
+            var_y=float(asteroid[1]-station[1])
             if(var_x != 0):
                 p=var_y/var_x
             elif(var_y > 0):
@@ -42,15 +43,19 @@ def asteroids_detected(asteroids,point):
 def get_best_asteroid(asteroids):
     solutions=[]
 
+    # Get detected asteroids for each posible station
     for asteroid in asteroids:
             solutions.append(asteroids_detected(asteroids,asteroid))
 
-    best_asteroid=solutions.index(np.max(solutions))
-    return [asteroids[best_asteroid],np.max(solutions)]
+    # Return best station and number of detected asteriods
+    max_detections=np.max(solutions)
+    best_asteroid=solutions.index(max_detections)
+    return [asteroids[best_asteroid],max_detections]
 
 
 
 # Examples
+print("Result for examples:")
 asteroid_map=[
     '.#..#',
     '.....',
@@ -58,7 +63,7 @@ asteroid_map=[
     '....#',
     '...##'
 ]
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 print(get_best_asteroid(asteroids))
 
 
@@ -74,7 +79,7 @@ asteroid_map=[
     '##...#..#.',
     '.#....####'
 ]
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 print(get_best_asteroid(asteroids))
 
 
@@ -90,7 +95,7 @@ asteroid_map=[
     '......#...',
     '.####.###.'
 ]
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 print(get_best_asteroid(asteroids))
 
 
@@ -106,7 +111,7 @@ asteroid_map=[
     '.##...##.#',
     '.....#.#..'
 ]
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 print(get_best_asteroid(asteroids))
 
 
@@ -132,20 +137,20 @@ asteroid_map=[
     '#.#.#.#####.####.###',
     '###.##.####.##.#..##'
 ]
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 print(get_best_asteroid(asteroids))
 
 
 # My puzzle
 print("Result for my puzzle:")
 # Load data
-file = open('data/input.data', 'r')
+file = open('./input.data', 'r')
 lines = file.readlines()
 asteroid_map=[line[:-1] for line in lines]
 
 # Calculate the solution
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 solution=get_best_asteroid(asteroids)
 
 # Print the solution
-print("Solution: "+solution.__str__())
+print(solution)
--- Day 10/part2.py
+++ Day 10/part2.py
@@ -5,26 +5,18 @@ def transform_coors(point):
     point.reverse()
     return point
 
-def get_point_laser(map):
+# Traduce map to get points in which object appear
+def get_points(map,object):
+    objects=[]
     rows=len(map)
     cols=len(map[0])
 
+    # Find object in each point of the map
     for x in range(rows):
         for y in range(cols):
-            if(map[x][y] == 'X'):
-                return [x,y]
-
-def get_point_asteroids(map):
-    asteroids=[]
-    rows=len(map)
-    cols=len(map[0])
-
-    for x in range(rows):
-        for y in range(cols):
-            if(map[x][y] == '#'):
-                asteroids.append([x,y])
-
-    return asteroids
+            if(map[x][y] == object):
+                objects.append([x,y])
+    return objects
 
 def vaporization(asteroids,laser):
     targets=[]
@@ -32,7 +24,7 @@ def vaporization(asteroids,laser):
 
     # Populate targets
     for asteroid in asteroids:
-        # Calculate down of straight
+        # Calculate straight
         var_x=float(asteroid[1]-laser[1])
         var_y=float(asteroid[0]-laser[0])
         if(var_x != 0):
@@ -42,57 +34,47 @@ def vaporization(asteroids,laser):
         else:
             p=float('-inf')
 
-        # Calculate direction
-        sign_x = '+' if var_x >= 0 else '-'
-        sign_y = '+' if var_y >= 0 else '-'
-        if(sign_y == '-' and sign_x == '+'):
-            key=1
-        elif(sign_y == '+' and sign_x == '+'):
-            key=2
-        elif(sign_y == '+' and sign_x == '-'):
-            key=3
-        elif(sign_y == '-' and sign_x == '-'):
-            key=4
+        # Calculate angle from straight with arctan(p)
+        angle=(np.degrees(np.arctan(p))+90)
+        if(var_x < 0):
+            angle += 180
 
         # Calculate distance
         distance=np.sqrt(pow(var_x,2)+pow(var_y,2))
 
         # Add target
-        targets.append([key,p,distance,asteroid])
+        targets.append([angle,distance,asteroid])
 
-    # Targets order by quadrant, straight and distance (down to up)
+    # Targets order by angle and distance (down to up)
     targets.sort()
 
     # Gen shoots
-    last_quadrant=last_straight=''
+    last_angle=''
     cont=0
     while(len(targets) > 0):
         # Get next target
         target=targets.pop(0)
-        cont+=1
 
-        # Verify if last target was in the same quadrant and straight
-        quadrant=str(target[0])
-        straight=str(target[1])
-        if(straight != last_straight or quadrant != last_quadrant):
-            last_straight=straight
-            last_quadrant=quadrant
-
-            cont-=1
+        # Verify if last target was in the same angle
+        angle=str(target[0])
+        if(angle != last_angle):
+            last_angle=angle
             shoots.append(target)
+            cont=0
         else:
             targets.append(target)
+            cont+=1
 
-        # If there are only targets from the same quadrant and straight
+        # If there are only targets from the same angle
         if(cont == len(targets)):
-            last_quadrant=last_straight=''
-            cont=0
+            last_angle=''
 
     return shoots
 
 
 
 # Examples
+print("Result for examples:")
 asteroid_map=[
     '.#....#####...#..',
     '##...##.#####..##',
@@ -100,8 +82,8 @@ asteroid_map=[
     '..#.....X...###..',
     '..#.#.....#....##'
 ]
-laser=get_point_laser(asteroid_map)
-asteroids=get_point_asteroids(asteroid_map)
+laser=get_points(asteroid_map,'X')[0]
+asteroids=get_points(asteroid_map,'#')
 shoots=vaporization(asteroids,laser)
 print(shoots)
 
@@ -127,35 +109,35 @@ asteroid_map=[
     '#.#.#.#####.####.###',
     '###.##.####.##.#..##'
 ]
-laser=get_point_laser(asteroid_map)
-asteroids=get_point_asteroids(asteroid_map)
+laser=get_points(asteroid_map,'X')[0]
+asteroids=get_points(asteroid_map,'#')
 shoots=vaporization(asteroids,laser)
-print(transform_coors(shoots[0][3]))
-print(transform_coors(shoots[1][3]))
-print(transform_coors(shoots[2][3]))
-print(transform_coors(shoots[9][3]))
-print(transform_coors(shoots[19][3]))
-print(transform_coors(shoots[49][3]))
-print(transform_coors(shoots[99][3]))
-print(transform_coors(shoots[198][3]))
-print(transform_coors(shoots[199][3]))
-print(transform_coors(shoots[200][3]))
-print(transform_coors(shoots[298][3]))
+print(transform_coors(shoots[0][2]))
+print(transform_coors(shoots[1][2]))
+print(transform_coors(shoots[2][2]))
+print(transform_coors(shoots[9][2]))
+print(transform_coors(shoots[19][2]))
+print(transform_coors(shoots[49][2]))
+print(transform_coors(shoots[99][2]))
+print(transform_coors(shoots[198][2]))
+print(transform_coors(shoots[199][2]))
+print(transform_coors(shoots[200][2]))
+print(transform_coors(shoots[298][2]))
 
 
 # My puzzle
 print("Result for my puzzle:")
 # Load data
-file = open('data/input.data', 'r')
+file = open('./input.data', 'r')
 lines = file.readlines()
 asteroid_map=[line[:-1] for line in lines]
 
 # Calculate the solution
 laser=[11, 11]
-asteroids=get_point_asteroids(asteroid_map)
+asteroids=get_points(asteroid_map,'#')
 asteroids.remove(laser)
 shoots=vaporization(asteroids,laser)
-solution=transform_coors(shoots[199][3])
+solution=transform_coors(shoots[199][2])
 
 # Print the solution
-print("Solution: "+solution.__str__())
+print(solution)
