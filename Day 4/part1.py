ac59ba646be33dc44c1e57267b087f8e2d307eff
fpuentes <fpuentes@ujaen.es>
2020-03-15 01:36:41 +0100

    Day 4. Clean up

--- Day 4/part1.py
+++ Day 4/part1.py
@@ -4,26 +4,22 @@ import numpy as np
 def eval_password(password):
     # Conditions
     is_increasing=True
-    duplicate_digits={}
+    has_duplicates=False
 
-    previous_digit=''
-    for digit in str(password):
+    previous_digit=password[0]
+    for digit in password[1:]:
+        # Verify increasing condition
         if(digit < previous_digit):
             is_increasing = False
             break
-        if(digit == previous_digit):
-            if(digit in duplicate_digits):
-                duplicate_digits[digit] += 1
-            else:
-                duplicate_digits.update({digit : 2})
+        # Verify duplicate condition
+        elif(digit == previous_digit):
+            has_duplicates=True
 
         previous_digit = digit
 
-    # If digits always increase and exists duplicates
-    if(is_increasing and len(duplicate_digits) != 0):
-        return True
-
-    return False
+    # If conditions are True
+    return is_increasing and has_duplicates
 
 def gen_password_range(min,max):
     valid_passwords = []
@@ -31,7 +27,7 @@ def gen_password_range(min,max):
     # Generate possible passwords
     for password in range(min, max + 1):
         # Eval password
-        if(eval_password(password)):
+        if(eval_password(str(password))):
             valid_passwords.append(password)
 
     return len(valid_passwords)
@@ -42,7 +38,7 @@ def gen_password_range(min,max):
 # My puzzle
 print("Result for my puzzle:")
 # Load data
-file = open('data/input.data', 'r')
+file = open('./input.data', 'r')
 lines = file.readlines()[0].split('-')
 min=int(lines[0])
 max=int(lines[1])
@@ -51,4 +47,4 @@ max=int(lines[1])
 solution=gen_password_range(min,max)
 
 # Print the solution
-print("Solution is: "+solution.__str__())
+print(solution)
--- Day 4/part2.py
+++ Day 4/part2.py
@@ -6,12 +6,13 @@ def eval_password(password):
     is_increasing=True
     duplicate_digits={}
 
-    previous_digit=''
-    for digit in str(password):
+    previous_digit=password[0]
+    for digit in password[1:]:
+        # Verify increasing condition
         if digit < previous_digit:
             is_increasing = False
             break
-        if digit == previous_digit:
+        elif digit == previous_digit:
             if digit in duplicate_digits:
                 duplicate_digits[digit] += 1
             else:
@@ -19,11 +20,8 @@ def eval_password(password):
 
         previous_digit = digit
 
-    # If digits always increase and exists duplicates of exactly two
-    if(is_increasing and 2 in [duplicate_digits[i] for i in duplicate_digits]):
-        return True
-
-    return False
+    # If conditions are True
+    return is_increasing and 2 in [duplicate_digits[i] for i in duplicate_digits]
 
 def gen_password_range(min,max):
     valid_passwords = []
@@ -31,7 +29,7 @@ def gen_password_range(min,max):
     # Generate possible passwords
     for password in range(min, max + 1):
         # Eval password
-        if(eval_password(password)):
+        if(eval_password(str(password))):
             valid_passwords.append(password)
 
     return len(valid_passwords)
@@ -42,7 +40,7 @@ def gen_password_range(min,max):
 # My puzzle
 print("Result for my puzzle:")
 # Load data
-file = open('data/input.data', 'r')
+file = open('./input.data', 'r')
 lines = file.readlines()[0].split('-')
 min=int(lines[0])
 max=int(lines[1])
@@ -51,4 +49,4 @@ max=int(lines[1])
 solution=gen_password_range(min,max)
 
 # Print the solution
-print("Solution is: "+solution.__str__())
+print(solution)
