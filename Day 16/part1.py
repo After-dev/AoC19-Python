295c79d11f2167dc715083636a644c411b4b83b9
fpuentes <fpuentes@ujaen.es>
2020-03-17 01:35:16 +0100

    Day 16. Clean up

--- Day 16/part1.py
+++ Day 16/part1.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def FFT(input,phases):
     base_pattern=[0,1,0,-1]
+    input=[int(i) for i in input]
 
     # Repeat several phases
     for phase in range(phases):
@@ -17,7 +18,7 @@ def FFT(input,phases):
             input[d]=abs(sum)%10
         #print('After '+str(phase)+' phase: '+str(input))
 
-    return input
+    return "".join(map(str, input[:8]))
 
 
 
@@ -28,20 +29,16 @@ def FFT(input,phases):
 
 # Examples
 print("Result for examples:")
-data='12345678'
-input=[int(i) for i in data]
+input='12345678'
 print(FFT(input,4))
 
-data='80871224585914546619083218645595'
-input=[int(i) for i in data]
+input='80871224585914546619083218645595'
 print(FFT(input,100))
 
-data='19617804207202209144916044189917'
-input=[int(i) for i in data]
+input='19617804207202209144916044189917'
 print(FFT(input,100))
 
-data='69317163492948606335995924319873'
-input=[int(i) for i in data]
+input='69317163492948606335995924319873'
 print(FFT(input,100))
 
 
@@ -52,8 +49,7 @@ print(FFT(input,100))
 print("Result for my puzzle:")
 # Load data
 file = open('./input.data', 'r')
-lines = file.readlines()[0][:-1]
-input=[int(i) for i in lines]
+input = file.readlines()[0][:-1]
 
 # Calculate the solution
 solution=FFT(input,100)
