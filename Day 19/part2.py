de9cec7d0cfc7720ccfd1fc91cbc53f8f9fd4c8e
fpuentes <fpuentes@ujaen.es>
2020-03-19 22:56:43 +0100

    Part 2 of day 19

---
+++ Day 19/part2.py
@@ -0,0 +1,147 @@
+import numpy as np
+
+
+def intcode_program(state):
+    output=[]
+
+    # Get state
+    intcode_aux=state[0][:]
+    inputs=state[1]
+    pointer=state[2]
+    relative_base=state[3]
+    max_size=state[4]
+    n_read=state[5]
+
+    # Resize array (get more memory)
+    intcode_aux=np.resize(intcode_aux,(max_size))
+
+    # Browse array by steps of 4
+    while pointer < len(intcode):
+        # Get opcode and mode of parameters
+        opcode_instruction=str(intcode_aux[pointer])
+        while(len(opcode_instruction) < 5):
+            opcode_instruction = '0'+opcode_instruction
+
+        opcode=int(opcode_instruction[-2:])
+        mode_params=[
+            int(opcode_instruction[-3]),
+            int(opcode_instruction[-4]),
+            int(opcode_instruction[-5])
+        ]
+
+        # Get index of each parameter
+        indexes=[-1,-1,-1]
+        for i in range(len(indexes)):
+            pos=pointer+1+i
+            if(pos < len(intcode_aux)):
+                if(mode_params[i] == 0):
+                    indexes[i]=intcode_aux[pos]
+                elif(mode_params[i] == 1):
+                    indexes[i]=pos
+                elif(mode_params[i] == 2):
+                    indexes[i]=relative_base+intcode_aux[pos]
+
+        # Apply opcode operation
+        # STOP
+        if(opcode == 99):
+            break
+
+        # ADDITION
+        elif(opcode == 1):
+            op1=intcode_aux[indexes[0]]
+            op2=intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]]=op1+op2
+            pointer+=4
+
+        # MULTIPLICATION
+        elif(opcode == 2):
+            op1=intcode_aux[indexes[0]]
+            op2=intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]]=op1*op2
+            pointer+=4
+
+        # INPUT
+        elif(opcode == 3):
+            intcode_aux[indexes[0]]=inputs.pop(0)
+            pointer+=2
+
+        # OUTPUT
+        elif(opcode == 4):
+            #print('Output: '+intcode_aux[indexes[0]].__str__())
+            output.append(intcode_aux[indexes[0]])
+            pointer+=2
+            if(len(output) == n_read):
+                break
+
+        # JUMP-IF-TRUE
+        elif(opcode == 5):
+            if(intcode_aux[indexes[0]] != 0):
+                pointer=intcode_aux[indexes[1]]
+            else:
+                pointer+=3
+
+        # JUMP-IF-FALSE
+        elif(opcode == 6):
+            if(intcode_aux[indexes[0]] == 0):
+                pointer=intcode_aux[indexes[1]]
+            else:
+                pointer+=3
+
+        # LESS-THAN
+        elif(opcode == 7):
+            if(intcode_aux[indexes[0]] < intcode_aux[indexes[1]]):
+                intcode_aux[indexes[2]]=1
+            else:
+                intcode_aux[indexes[2]]=0
+            pointer+=4
+
+        # EQUAL-TO
+        elif(opcode == 8):
+            if(intcode_aux[indexes[0]] == intcode_aux[indexes[1]]):
+                intcode_aux[indexes[2]]=1
+            else:
+                intcode_aux[indexes[2]]=0
+            pointer+=4
+
+        # ADJUST RELATIVE BASE
+        elif(opcode == 9):
+            relative_base += intcode_aux[indexes[0]]
+            pointer+=2
+
+    return output,intcode_aux,pointer,relative_base
+
+
+def get_rectangle(intcode,size_x,size_y):
+    O=(0,0)
+
+    while(True):
+        # Move bottom-left corner to a first element of beam
+        while(intcode_program([intcode,[O[0],O[1]+size_y-1],0,0,1000,1])[0][0] == 0):
+            O=(O[0]+1,O[1])
+
+        # Verify top-right corner
+        if(intcode_program([intcode,[O[0]+size_x-1,O[1]],0,0,1000,1])[0][0] == 1):
+            break
+
+        # Try next y
+        O=(O[0],O[1]+1)
+
+    return O
+
+
+
+
+
+
+# My puzzle
+print("Result for my puzzle:")
+# Load data
+file = open('./input.data', 'r')
+line = file.readlines()[0][:-1].split(',')
+intcode=[int(i) for i in line]
+
+# Calculate the solution
+solution=get_rectangle(intcode,100,100)
+
+# Print the solution
+print(solution[0]*10000+solution[1])
