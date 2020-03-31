4ad138bc788ef214e330bfa13258cfa9c440c8aa
fpuentes <fpuentes@ujaen.es>
2020-03-31 20:02:01 +0200

    Day 8. Clean up

--- Day 8/part1.py
+++ Day 8/part1.py
@@ -1,17 +1,17 @@
-def get_layers(image_data,image_dims,output_format='1d'):
+def get_layers(image_data, image_dims, output_format='1d'):
     # Calculate layer total size
-    layer_size=image_dims[0]*image_dims[1]
+    layer_size = image_dims[0]*image_dims[1]
 
     # Split all data in different layers (1D)
-    layers_1d=[image_data[i:i+layer_size] for i in range(0,len(image_data),layer_size)]
-    if(output_format == '1d'):
+    layers_1d = [image_data[i:i+layer_size] for i in range(0, len(image_data), layer_size)]
+    if output_format == '1d':
         return layers_1d
 
     # Split each layer in rows (2D)
-    layers_2d=[]
-    row_size=layer_size/image_dims[1]
+    layers_2d = []
+    row_size = layer_size/image_dims[1]
     for layer_1d in layers_1d:
-        layer_2d=[layer_1d[i:i+row_size] for i in range(0,len(layer_1d),row_size)]
+        layer_2d = [layer_1d[i:i+row_size] for i in range(0, len(layer_1d), row_size)]
         layers_2d.append(layer_2d)
 
     return layers_2d
@@ -23,9 +23,9 @@ def get_layers(image_data,image_dims,output_format='1d'):
 
 # Examples
 print("Result for examples:")
-image_data='123456789012'
-image_dims=[3,2]
-print(get_layers(image_data,image_dims,'2d'))
+image_data = '123456789012'
+image_dims = [3, 2]
+print(get_layers(image_data, image_dims, '2d'))
 
 
 
@@ -34,22 +34,22 @@ print("Result for my puzzle:")
 # Load data
 file = open('./input.data', 'r')
 image_data = file.readlines()[0][:-1]
-image_dims=[25,6]
+image_dims = [25, 6]
 
 # Calculate the solution
-layers=get_layers(image_data,image_dims)
+layers = get_layers(image_data, image_dims)
 
-fewest_zeros=image_dims[0]*image_dims[1]
-fewest_layer=-1
+fewest_zeros = image_dims[0]*image_dims[1]
+fewest_layer = -1
 for layer in layers:
-    n_zeros=layer.count('0')
-    if(n_zeros < fewest_zeros):
-        fewest_zeros=n_zeros
-        fewest_layer=layer
+    n_zeros = layer.count('0')
+    if n_zeros < fewest_zeros:
+        fewest_zeros = n_zeros
+        fewest_layer = layer
 
-ones=fewest_layer.count('1')
-twos=fewest_layer.count('2')
-solution=ones*twos
+ones = fewest_layer.count('1')
+twos = fewest_layer.count('2')
+solution = ones*twos
 
 # Print the solution
 print(solution)
--- Day 8/part2.py
+++ Day 8/part2.py
@@ -1,36 +1,36 @@
-def get_layers(image_data,image_dims,output_format='1d'):
+def get_layers(image_data, image_dims, output_format='1d'):
     # Calculate layer total size
-    layer_size=image_dims[0]*image_dims[1]
+    layer_size = image_dims[0]*image_dims[1]
 
     # Split all data in different layers (1D)
-    layers_1d=[image_data[i:i+layer_size] for i in range(0,len(image_data),layer_size)]
-    if(output_format == '1d'):
+    layers_1d = [image_data[i:i+layer_size] for i in range(0, len(image_data), layer_size)]
+    if output_format == '1d':
         return layers_1d
 
     # Split each layer in rows (2D)
-    layers_2d=[]
-    row_size=layer_size/image_dims[1]
+    layers_2d = []
+    row_size = layer_size/image_dims[1]
     for layer_1d in layers_1d:
-        layer_2d=[layer_1d[i:i+row_size] for i in range(0,len(layer_1d),row_size)]
+        layer_2d = [layer_1d[i:i+row_size] for i in range(0, len(layer_1d), row_size)]
         layers_2d.append(layer_2d)
 
     return layers_2d
 
 
-def decode_image(layers,image_dims):
-    image_decoded=[]
+def decode_image(layers, image_dims):
+    image_decoded = []
 
     # Search color for each pixel
     for row in range(image_dims[1]):
-        row_total=''
+        row_total = ''
         for col in range(image_dims[0]):
             # Compare same pixel of each layer, from first to last
-            color=''
+            color = ''
             for layer in layers:
-                color=layer[row][col]
+                color = layer[row][col]
 
                 # If current pixel's color is not transparent, break
-                if(color != '2'):
+                if color != '2':
                     break
 
             row_total += color
@@ -45,10 +45,10 @@ def decode_image(layers,image_dims):
 
 # Examples
 print("Result for examples:")
-image_data='0222112222120000'
-image_dims=[2,2]
-layers=get_layers(image_data,image_dims,'2d')
-print(decode_image(layers,image_dims))
+image_data = '0222112222120000'
+image_dims = [2, 2]
+layers = get_layers(image_data, image_dims, '2d')
+print(decode_image(layers, image_dims))
 
 
 
@@ -57,12 +57,12 @@ print("Result for my puzzle:")
 # Load data
 file = open('./input.data', 'r')
 image_data = file.readlines()[0][:-1]
-image_dims=[25,6]
+image_dims = [25, 6]
 
 # Calculate the solution
-layers=get_layers(image_data,image_dims,'2d')
-image_decoded=decode_image(layers,image_dims)
+layers = get_layers(image_data, image_dims, '2d')
+image_decoded = decode_image(layers, image_dims)
 
 # Print the solution
 for row in image_decoded:
-    print(row.replace('0',' ').replace('1','#'))
+    print(row.replace('0', ' ').replace('1', '#'))
