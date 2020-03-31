a8fdb4f8b3f4d487800b42bbdacf4ff6110484bc
fpuentes <fpuentes@ujaen.es>
2020-03-31 19:37:40 +0200

    Day 5. Clean up

--- Day 5/part1.py
+++ Day 5/part1.py
@@ -1,24 +1,24 @@
-def intcode_program(intcode,input):
+def intcode_program(intcode, input):
     # Copy array
-    intcode_aux=intcode[:]
+    intcode_aux = intcode[:]
 
     # Browse array by steps
-    pointer=0
+    pointer = 0
     while pointer < len(intcode_aux):
         # Get opcode and mode of parameters
-        opcode_instruction=str(intcode_aux[pointer])
-        while(len(opcode_instruction) < 5):
+        opcode_instruction = str(intcode_aux[pointer])
+        while len(opcode_instruction) < 5:
             opcode_instruction = '0'+opcode_instruction
 
-        opcode=int(opcode_instruction[-2:])
-        mode_params=[
+        opcode = int(opcode_instruction[-2:])
+        mode_params = [
             int(opcode_instruction[-3]),
             int(opcode_instruction[-4]),
             int(opcode_instruction[-5])
         ]
 
         # Get index of each parameter
-        indexes=[
+        indexes = [
             intcode_aux[pointer+1] if mode_params[0] == 0 else pointer+1,
             intcode_aux[pointer+2] if mode_params[1] == 0 else pointer+2,
             intcode_aux[pointer+3] if mode_params[2] == 0 else pointer+3
@@ -26,33 +26,33 @@ def intcode_program(intcode,input):
 
         # Apply opcode operation
         # STOP
-        if(opcode == 99):
+        if opcode == 99:
             break
 
         # ADDITION
-        elif(opcode == 1):
-            op1=intcode_aux[indexes[0]]
-            op2=intcode_aux[indexes[1]]
-            intcode_aux[indexes[2]]=op1+op2
-            pointer+=4
+        elif opcode == 1:
+            op1 = intcode_aux[indexes[0]]
+            op2 = intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]] = op1+op2
+            pointer += 4
 
         # MULTIPLICATION
-        elif(opcode == 2):
-            op1=intcode_aux[indexes[0]]
-            op2=intcode_aux[indexes[1]]
-            intcode_aux[indexes[2]]=op1*op2
-            pointer+=4
+        elif opcode == 2:
+            op1 = intcode_aux[indexes[0]]
+            op2 = intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]] = op1*op2
+            pointer += 4
 
         # INPUT
-        elif(opcode == 3):
-            intcode_aux[indexes[0]]=input
-            pointer+=2
+        elif opcode == 3:
+            intcode_aux[indexes[0]] = input
+            pointer += 2
 
         # OUTPUT
-        elif(opcode == 4):
+        elif opcode == 4:
             #print('Output: '+intcode_aux[indexes[0]].__str__())
-            intcode_aux[0]=intcode_aux[indexes[0]]
-            pointer+=2
+            intcode_aux[0] = intcode_aux[indexes[0]]
+            pointer += 2
 
     return intcode_aux[0]
 
@@ -63,10 +63,10 @@ def intcode_program(intcode,input):
 print("Result for my puzzle:")
 # Load data
 file = open('./input.data', 'r')
-intcode=[int(i) for i in file.readlines()[0][:-1].split(',')]
+intcode = [int(i) for i in file.readlines()[0][:-1].split(',')]
 
 # Calculate the solution
-solution=intcode_program(intcode,1)
+solution = intcode_program(intcode, 1)
 
 # Print the solution
 print(solution)
--- Day 5/part2.py
+++ Day 5/part2.py
@@ -1,24 +1,24 @@
-def intcode_program(intcode,input):
+def intcode_program(intcode, input):
     # Copy array
-    intcode_aux=intcode[:]
+    intcode_aux = intcode[:]
 
     # Browse array by steps
-    pointer=0
+    pointer = 0
     while pointer < len(intcode_aux):
         # Get opcode and mode of parameters
-        opcode_instruction=str(intcode_aux[pointer])
-        while(len(opcode_instruction) < 5):
+        opcode_instruction = str(intcode_aux[pointer])
+        while len(opcode_instruction) < 5:
             opcode_instruction = '0'+opcode_instruction
 
-        opcode=int(opcode_instruction[-2:])
-        mode_params=[
+        opcode = int(opcode_instruction[-2:])
+        mode_params = [
             int(opcode_instruction[-3]),
             int(opcode_instruction[-4]),
             int(opcode_instruction[-5])
         ]
 
         # Get index of each parameter
-        indexes=[
+        indexes = [
             intcode_aux[pointer+1] if mode_params[0] == 0 and pointer+1 < len(intcode_aux) else pointer+1,
             intcode_aux[pointer+2] if mode_params[1] == 0 and pointer+2 < len(intcode_aux) else pointer+2,
             intcode_aux[pointer+3] if mode_params[2] == 0 and pointer+3 < len(intcode_aux) else pointer+3
@@ -26,63 +26,63 @@ def intcode_program(intcode,input):
 
         # Apply opcode operation
         # STOP
-        if(opcode == 99):
+        if opcode == 99:
             break
 
         # ADDITION
-        elif(opcode == 1):
-            op1=intcode_aux[indexes[0]]
-            op2=intcode_aux[indexes[1]]
-            intcode_aux[indexes[2]]=op1+op2
-            pointer+=4
+        elif opcode == 1:
+            op1 = intcode_aux[indexes[0]]
+            op2 = intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]] = op1+op2
+            pointer += 4
 
         # MULTIPLICATION
-        elif(opcode == 2):
-            op1=intcode_aux[indexes[0]]
-            op2=intcode_aux[indexes[1]]
-            intcode_aux[indexes[2]]=op1*op2
-            pointer+=4
+        elif opcode == 2:
+            op1 = intcode_aux[indexes[0]]
+            op2 = intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]] = op1*op2
+            pointer += 4
 
         # INPUT
-        elif(opcode == 3):
-            intcode_aux[indexes[0]]=input
-            pointer+=2
+        elif opcode == 3:
+            intcode_aux[indexes[0]] = input
+            pointer += 2
 
         # OUTPUT
-        elif(opcode == 4):
+        elif opcode == 4:
             #print('Output: '+intcode_aux[indexes[0]].__str__())
-            intcode_aux[0]=intcode_aux[indexes[0]]
-            pointer+=2
+            intcode_aux[0] = intcode_aux[indexes[0]]
+            pointer += 2
 
         # JUMP-IF-TRUE
-        elif(opcode == 5):
-            if(intcode_aux[indexes[0]] != 0):
-                pointer=intcode_aux[indexes[1]]
+        elif opcode == 5:
+            if intcode_aux[indexes[0]] != 0:
+                pointer = intcode_aux[indexes[1]]
             else:
-                pointer+=3
+                pointer += 3
 
         # JUMP-IF-FALSE
-        elif(opcode == 6):
-            if(intcode_aux[indexes[0]] == 0):
-                pointer=intcode_aux[indexes[1]]
+        elif opcode == 6:
+            if intcode_aux[indexes[0]] == 0:
+                pointer = intcode_aux[indexes[1]]
             else:
-                pointer+=3
+                pointer += 3
 
         # LESS-THAN
-        elif(opcode == 7):
-            if(intcode_aux[indexes[0]] < intcode_aux[indexes[1]]):
-                intcode_aux[indexes[2]]=1
+        elif opcode == 7:
+            if intcode_aux[indexes[0]] < intcode_aux[indexes[1]]:
+                intcode_aux[indexes[2]] = 1
             else:
-                intcode_aux[indexes[2]]=0
-            pointer+=4
+                intcode_aux[indexes[2]] = 0
+            pointer += 4
 
         # EQUAL-TO
-        elif(opcode == 8):
-            if(intcode_aux[indexes[0]] == intcode_aux[indexes[1]]):
-                intcode_aux[indexes[2]]=1
+        elif opcode == 8:
+            if intcode_aux[indexes[0]] == intcode_aux[indexes[1]]:
+                intcode_aux[indexes[2]] = 1
             else:
-                intcode_aux[indexes[2]]=0
-            pointer+=4
+                intcode_aux[indexes[2]] = 0
+            pointer += 4
 
     return intcode_aux[0]
 
@@ -94,10 +94,10 @@ def intcode_program(intcode,input):
 print("Result for my puzzle:")
 # Load data
 file = open('./input.data', 'r')
-intcode=[int(i) for i in file.readlines()[0][:-1].split(',')]
+intcode = [int(i) for i in file.readlines()[0][:-1].split(',')]
 
 # Calculate the solution
-solution=intcode_program(intcode,5)
+solution = intcode_program(intcode, 5)
 
 # Print the solution
 print(solution)
