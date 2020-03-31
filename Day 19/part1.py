0ba7e966a199ef102caa98294a6f5ecc7333ffa9
fpuentes <fpuentes@ujaen.es>
2020-03-31 22:03:54 +0200

    Day 19. Clean up

--- Day 19/part1.py
+++ Day 19/part1.py
@@ -1,128 +1,126 @@
 import numpy as np
 
 
-def intcode_program(state):
-    output=[]
+def intcode_program(state, max_size, n_read):
+    output = []
 
     # Get state
-    intcode_aux=state[0][:]
-    inputs=state[1]
-    pointer=state[2]
-    relative_base=state[3]
-    max_size=state[4]
-    n_read=state[5]
+    intcode_aux = state[0][:]
+    inputs = state[1]
+    pointer = state[2]
+    relative_base = state[3]
 
     # Resize array (get more memory)
-    intcode_aux=np.resize(intcode_aux,(max_size))
+    intcode_aux = np.resize(intcode_aux, max_size)
 
     # Browse array by steps of 4
     while pointer < len(intcode):
         # Get opcode and mode of parameters
-        opcode_instruction=str(intcode_aux[pointer])
-        while(len(opcode_instruction) < 5):
+        opcode_instruction = str(intcode_aux[pointer])
+        while len(opcode_instruction) < 5:
             opcode_instruction = '0'+opcode_instruction
 
-        opcode=int(opcode_instruction[-2:])
-        mode_params=[
+        opcode = int(opcode_instruction[-2:])
+        mode_params = [
             int(opcode_instruction[-3]),
             int(opcode_instruction[-4]),
             int(opcode_instruction[-5])
         ]
 
         # Get index of each parameter
-        indexes=[-1,-1,-1]
+        indexes = [-1, -1, -1]
         for i in range(len(indexes)):
-            pos=pointer+1+i
-            if(pos < len(intcode_aux)):
-                if(mode_params[i] == 0):
-                    indexes[i]=intcode_aux[pos]
-                elif(mode_params[i] == 1):
-                    indexes[i]=pos
-                elif(mode_params[i] == 2):
-                    indexes[i]=relative_base+intcode_aux[pos]
+            pos = pointer+1+i
+            if pos < len(intcode_aux):
+                if mode_params[i] == 0:
+                    indexes[i] = intcode_aux[pos]
+                elif mode_params[i] == 1:
+                    indexes[i] = pos
+                elif mode_params[i] == 2:
+                    indexes[i] = relative_base+intcode_aux[pos]
 
         # Apply opcode operation
         # STOP
-        if(opcode == 99):
+        if opcode == 99:
             break
 
         # ADDITION
-        elif(opcode == 1):
-            op1=intcode_aux[indexes[0]]
-            op2=intcode_aux[indexes[1]]
-            intcode_aux[indexes[2]]=op1+op2
-            pointer+=4
+        elif opcode == 1:
+            op1 = intcode_aux[indexes[0]]
+            op2 = intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]] = op1+op2
+            pointer += 4
 
         # MULTIPLICATION
-        elif(opcode == 2):
-            op1=intcode_aux[indexes[0]]
-            op2=intcode_aux[indexes[1]]
-            intcode_aux[indexes[2]]=op1*op2
-            pointer+=4
+        elif opcode == 2:
+            op1 = intcode_aux[indexes[0]]
+            op2 = intcode_aux[indexes[1]]
+            intcode_aux[indexes[2]] = op1*op2
+            pointer += 4
 
         # INPUT
-        elif(opcode == 3):
-            intcode_aux[indexes[0]]=inputs.pop(0)
-            pointer+=2
+        elif opcode == 3:
+            intcode_aux[indexes[0]] = inputs.pop(0)
+            pointer += 2
 
         # OUTPUT
-        elif(opcode == 4):
+        elif opcode == 4:
             #print('Output: '+intcode_aux[indexes[0]].__str__())
             output.append(intcode_aux[indexes[0]])
-            pointer+=2
-            if(len(output) == n_read):
+            pointer += 2
+            if len(output) == n_read:
                 break
 
         # JUMP-IF-TRUE
-        elif(opcode == 5):
-            if(intcode_aux[indexes[0]] != 0):
-                pointer=intcode_aux[indexes[1]]
+        elif opcode == 5:
+            if intcode_aux[indexes[0]] != 0:
+                pointer = intcode_aux[indexes[1]]
             else:
-                pointer+=3
+                pointer += 3
 
         # JUMP-IF-FALSE
-        elif(opcode == 6):
-            if(intcode_aux[indexes[0]] == 0):
-                pointer=intcode_aux[indexes[1]]
+        elif opcode == 6:
+            if intcode_aux[indexes[0]] == 0:
+                pointer = intcode_aux[indexes[1]]
             else:
-                pointer+=3
+                pointer += 3
 
         # LESS-THAN
-        elif(opcode == 7):
-            if(intcode_aux[indexes[0]] < intcode_aux[indexes[1]]):
-                intcode_aux[indexes[2]]=1
+        elif opcode == 7:
+            if intcode_aux[indexes[0]] < intcode_aux[indexes[1]]:
+                intcode_aux[indexes[2]] = 1
             else:
-                intcode_aux[indexes[2]]=0
-            pointer+=4
+                intcode_aux[indexes[2]] = 0
+            pointer += 4
 
         # EQUAL-TO
-        elif(opcode == 8):
-            if(intcode_aux[indexes[0]] == intcode_aux[indexes[1]]):
-                intcode_aux[indexes[2]]=1
+        elif opcode == 8:
+            if intcode_aux[indexes[0]] == intcode_aux[indexes[1]]:
+                intcode_aux[indexes[2]] = 1
             else:
-                intcode_aux[indexes[2]]=0
-            pointer+=4
+                intcode_aux[indexes[2]] = 0
+            pointer += 4
 
         # ADJUST RELATIVE BASE
-        elif(opcode == 9):
+        elif opcode == 9:
             relative_base += intcode_aux[indexes[0]]
-            pointer+=2
+            pointer += 2
 
-    return output,intcode_aux,pointer,relative_base
+    return [output, intcode_aux, pointer, relative_base]
 
 
-def get_affected_points(intcode,range_x,range_y):
-    affected_points=[]
+def get_affected_points(intcode, range_x, range_y):
+    affected_points = []
 
     # Evaluate each point
     for x in range(range_x):
         for y in range(range_y):
             # Get output for that point
-            [output,_,_,_]=intcode_program([intcode,[x,y],0,0,1000,1])
+            [output, _, _, _]=intcode_program([intcode, [x, y], 0, 0], 1000, 1)
 
             # Verify output
-            if(output[0] == 1):
-                affected_points.append((x,y))
+            if output[0] == 1:
+                affected_points.append((x, y))
 
     return affected_points
 
@@ -136,10 +134,10 @@ print("Result for my puzzle:")
 # Load data
 file = open('./input.data', 'r')
 line = file.readlines()[0][:-1].split(',')
-intcode=[int(i) for i in line]
+intcode = [int(i) for i in line]
 
 # Calculate the solution
-solution=get_affected_points(intcode,50,50)
+solution = get_affected_points(intcode, 50, 50)
 
 # Print the solution
 print(len(solution))
